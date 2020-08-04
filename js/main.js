dcda63064cf23b249245491fadb2643164e9b0dd
TimurSagaidakov <66359248+TimurSagaidakov@users.noreply.github.com>
2020-08-04 20:15:33 +0500

    Доделал меню и начал верстать слайдер баннеров, сделал 1 баннер

--- js/main.js
+++ js/main.js
@@ -6,15 +6,23 @@ function closeCatalog(){
 }
 function funcServices(){
     document.querySelector('.header-navigation-services').style.display = 'block';
+    document.querySelector('.header-navigation-services').classList.add('services-open');
 }
 function closeServices(){
-    document.querySelector('.header-navigation-services').style.display = 'none';
+    if(window.innerWidth >768){
+        document.querySelector('.header-navigation-services').style.display = 'none';
+    }    
+    document.querySelector('.header-navigation-services').classList.remove('services-open');
 }
 function funcBuyer(){
     document.querySelector('.header-navigation-buyer').style.display = 'block';
+    document.querySelector('.header-navigation-buyer').classList.add('buyer-open');
 }
 function closeBuyer(){
-    document.querySelector('.header-navigation-buyer').style.display = 'none';
+    if(window.innerWidth >768){
+        document.querySelector('.header-navigation-buyer').style.display = 'none';
+    }  
+    document.querySelector('.header-navigation-buyer').classList.remove('buyer-open');
 }
 function menuMobileOpen(){
     document.querySelector('.header-navigation').classList.toggle('open');
@@ -22,6 +30,7 @@ function menuMobileOpen(){
     let mobilePoint = document.querySelectorAll('.header-navigation-li')
     for (let i = 0; i< mobilePoint.length; i++)
         mobilePoint[i].classList.toggle('move');
-    
+    document.querySelector('.header-navigation-buyer').classList.remove('buyer-open');
+    document.querySelector('.header-navigation-services').classList.remove('services-open');
 };
 
