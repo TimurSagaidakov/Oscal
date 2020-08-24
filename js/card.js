755ff6009c7619a75757db8edb3a90e4c6e517ff
TimurSagaidakov <66359248+TimurSagaidakov@users.noreply.github.com>
2020-08-24 17:38:01 +0500

    Заканчиваю верстать карточку товара

--- js/card.js
+++ js/card.js
@@ -1,13 +1,40 @@
+if(window.innerWidth > 769){
+  document.querySelectorAll('.tab-navigation__link').forEach((item)=>
+  item.addEventListener('click', function(e){
+    e.preventDefault();
+    const id = e.target.getAttribute('href').replace('#', '');
+    const tabLinkLine = e.target.querySelector('.tab-navigation__link span');
+    document.querySelectorAll('.tab-navigation__link').forEach((child) => child.classList.remove('display')
+    );
+    document.querySelectorAll('.tab-navigation__link span').forEach((child) => child.style.opacity = '0'
+    );
+    document.querySelectorAll('.tab-body-page').forEach((child) => child.classList.remove('display')
+    );
+    item.classList.add('display');
+    tabLinkLine.style.opacity = '1'
+    document.getElementById(id).classList.add('display');
+  })
+);
+}
+document.querySelector('.tab-navigation__link').click();
 $(document).ready(function () {
   $('.complect-color-slider').slick({
-    infinite: false,
+    infinite: true,
     slidesToShow: 6,
     slidesToScroll: 1,
     nextArrow: $('.complect-color-arrow__next'),
     prevArrow: $('.complect-color-arrow__prev'),
+    responsive:[
+      {
+          breakpoint: 998,
+          settings: {
+              slidesToShow: 4,
+          },
+      },
+    ]
   });
   $('.complect-door-slider').slick({
-    infinite: false,
+    infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     nextArrow: $('.complect-door-arrow__next'),
@@ -16,15 +43,39 @@ $(document).ready(function () {
       {
           breakpoint: 768,
           settings: {
-              slidesToShow: 1,
+              slidesToShow: 2,
           },
       },
+  ]
+  });
+  $('.complect-related-hand-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: $('.complect-related-hand-arrow__next'),
+    prevArrow: $('.complect-related-hand-arrow__prev'),
+    responsive:[
       {
           breakpoint: 576,
-          settings:{
-              slidesToShow: 1
-          }           
-      },
+          settings: {
+              slidesToShow: 2,
+          },
+      }
+  ]
+  });
+  $('.complect-related-lock-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: $('.complect-related-lock-arrow__next'),
+    prevArrow: $('.complect-related-lock-arrow__prev'),
+    responsive:[
+      {
+          breakpoint: 576,
+          settings: {
+              slidesToShow: 2,
+          },
+      }
   ]
   });
 })
